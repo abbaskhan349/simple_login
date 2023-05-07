5f0eadfc10bf8312713354c7c9267c200eb55662
Muhammad Abbas Khan <mabbas349@gmail.com>
2023-05-07 16:00:39 +0500

    adding responce

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,88 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginForm from './components/LoginForm';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import HomePage from './pages/HomePage';
+// import LoginForm from './components/LoginForm';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/home" element={<HomePage />} />
+//         <Route path="/" element={<LoginForm/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [responseData, setResponseData] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        { username, password }
+      );
+      console.log(response);
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleGetData = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      console.log(response);
+      setResponseData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<LoginForm/>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      {!isLoggedIn ? (
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      ) : (
+        <div>
+          <p>Successfully logged in!</p>
+          <button onClick={handleGetData}>Get Data</button>
+          {responseData && (
+            <div>
+              <p>Response from the server:</p>
+              <pre>{JSON.stringify(responseData, null, 2)}</pre>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
 export default App;
-
